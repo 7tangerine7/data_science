fab3547028fbe1a25e09eea3b619956c113ee6a1
7tangerine7 <ptukhap@gmail.com>
2020-04-27 23:59:34 +0300

    new

--- ds_lab2_2/ds_lab2_2/ds_lab2_2.py
+++ ds_lab2_2/ds_lab2_2/ds_lab2_2.py
@@ -1,6 +1,6 @@
 from spyre import server
-import vhi
-#server.include_df_index = True
+from vhi import general, analyze, month
+server.include_df_index = True
 
 
 class StockExample(server.App):
@@ -19,16 +19,16 @@ class StockExample(server.App):
     },
         {"type": 'dropdown',
         "label": 'region',
-        "options": [{"label": '%s'%(x), "value": '%s'%(x)} for x in range(1, 26)],
+        "options": [{"label":str(x), "value": str(x)} for x in range(1, 26)],
         "value": 'VHI',
         "key": 'region'},
         {"type":'dropdown',
         "label": 'start',
-        "options" : [{"label": '%s'%(x), "value": '%s'%(x)} for x in range(1, 53)],
+        "options" : [{"label": str(x), "value": str(x)} for x in range(1, 53)],
         "key": 'week_s'},
         {"type":'dropdown',
         "label": 'end',
-        "options" : [{"label": '%s'%(x), "value": '%s'%(x)} for x in range(1, 53)],
+        "options" : [{"label": str(x), "value": str(x)} for x in range(1, 53)],
         "key": 'week_e'}]
 
     controls = [{
@@ -37,34 +37,61 @@ class StockExample(server.App):
         "label": "plot"
     }]
 
-    tabs = ["Table","Plot"]
+    tabs = ["Table","Plot","Table2","Plot2"]
 
     outputs = [
         {
             "type": "plot",
-            "id": "plot",
+            "id": "plot_id",
             "control_id": "update_data",
             "tab": "Plot"},
+            {
+            "type": "plot",
+            "id": "plot_id2",
+            "control_id": "update_data",
+            "tab": "Plot2"},
         {
             "type": "table",
             "id": "table_id",
             "control_id": "update_data",
             "tab": "Table"
+        },
+         {
+            "type": "table",
+            "id": "table_id2",
+            "control_id": "update_data",
+            "tab": "Table2"
         }
+
     ]
 
-    def getData(self, params):
+    def table_id(self, params):
         region=params['region']
         time_series=params['time_series']
         start=params['week_s']
         end=params['week_e']
-        df=vhi.general(region)
+        df=general(region)
         df=df[(df['week']<=float(end)) & (df['week']>=float(start))]
         return df[[time_series]]
       
+    def table_id2(self, params):
+        region=params['region']
+        time_series=params['time_series']
+        start=params['week_s']
+        end=params['week_e']
+        df=general(region)
+        return  month(df)
 
-    def getPlot(self, params):
-        df = self.getData(params)
+    def plot_id(self, params):
+        df = self.table_id(params)
+        plt_obj = df.plot()
+        plt_obj.set_ylabel("Data")
+        plt_obj.set_xlabel("")
+        plt_obj.set_title(params['region'])
+        return plt_obj.get_figure()
+
+    def plot_id2(self, params):
+        df = self.table_id2(params)
         plt_obj = df.plot()
         plt_obj.set_ylabel("Data")
         plt_obj.set_xlabel("")
--- ds_lab2_2/ds_lab2_2/vhi.py
+++ ds_lab2_2/ds_lab2_2/vhi.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[5]:
-
-
-
-from urllib import request
-import time
-import datetime
 import os
 import pandas as pd
 def general(x):
@@ -40,27 +31,19 @@ def year_reg():
     return data3
 
 
-def month():
+def month(df):
     data4=df
     data4['month']=(df['week']/(4.34)).astype('int')+1
     #data4['month']=data4['month'].astype('int')+1
-    data4=data4.groupby(['month','year'], group_keys=False).apply(lambda x: pd.Series(dict(VHI_max=(x.VHI).max(), VHI_min=(x.VHI).min())))
+    data4=data4.groupby(['month'], group_keys=False).apply(lambda x: pd.Series(dict(VHI_max=(x.VHI).max(), VHI_min=(x.VHI).min())))
     #data4=data4.groupby(['month','year'], group_keys=False).apply()
+    return data4
 
 
 
 # In[55]:
 
 
-def year_max():
-    data4=df.groupby(['month'], group_keys=False).apply(lambda x: x.nlargest(1, 'VHI'))
-    data4=data4[['month','year']]
-    data4=data4.rename(columns={"year": "year_max"})
-    data5=df.groupby(['month'], group_keys=False).apply(lambda x: x.nsmallest(1, 'VHI'))
-    data5=data5[['month','year']]
-    data5=data5.rename(columns={"year": "year_min"})
-    data5=data5.merge(data4) 
-    return data5
 
 
 # In[11]:
